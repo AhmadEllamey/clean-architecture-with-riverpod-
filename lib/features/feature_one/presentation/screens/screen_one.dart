1307e23382c9e68c05307b695f5780ea26000e8e
ahmadellamey <ahmadellamey1@gmail.com>
2024-04-19 17:21:01 +0200

    [Feature] add screen one details

--- lib/features/feature_one/presentation/screens/screen_one.dart
+++ lib/features/feature_one/presentation/screens/screen_one.dart
@@ -1,3 +1,5 @@
+import 'package:clean_architecture_demo/features/feature_one/presentation/states/one_state.dart';
+import 'package:clean_architecture_demo/features/feature_one/presentation/view_models/one_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -9,6 +11,15 @@ class ScreenOne extends ConsumerStatefulWidget {
 }
 
 class _ScreenOneState extends ConsumerState<ScreenOne> {
+  /// this is the screen provider and it's scoop
+
+  final provider = StateNotifierProvider<OneViewModel, OneState>(
+    (ref) {
+      return OneViewModel(
+        OneState(title: "Hello"),
+      );
+    },
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +27,17 @@ class _ScreenOneState extends ConsumerState<ScreenOne> {
       body: Container(
         height: double.infinity,
         width: double.infinity,
-        color: Colors.purple,
+        color: Colors.white,
+        child: Center(
+          child: Text(
+            ref.watch(provider).title,
+            style: const TextStyle(
+              fontSize: 48,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+        ),
       ),
     );
   }
